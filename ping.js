69070f6e16627b656db73deea42b7de40f641d46
Phantom <76847497+Git-Phantom@users.noreply.github.com>
2021-01-06 14:47:22 -0500

    Console Update

--- ping.js
+++ ping.js
@@ -1,16 +1,26 @@
-// ShellJS Area
-var shell = require('shelljs')
-
-shell.exec(`z="";Az='ping';Cz=' blo';Ez='io';Dz='ble.';Bz=' -w5'; eval "$Az$Bz$Cz$Dz$Ez"`)
-
-
-// Ping Area
 var ping = require('ping')
 
 var hosts = ['127.0.0.1', '34.208.242.121', '44.241.80.149']
 hosts.forEach(function(host){
     ping.sys.probe(host, function(isAlive){
-        var msg = isAlive ? '[PHANTOM] ' + host + ' is alive' : '[PHANTOM] ' + host + ' is dead';
-        console.log('\x1b[36m%s\x1b[0m', msg)
+        var msg = isAlive ? '\x1b[32m[PHANTOM] ' + host + ' is On\x1b[0m' : '\x1b[31m[PHANTOM] ' + host + ' is Off\x1b[0m';
+        console.log('', msg)
     })
 })
+var shell = require('shelljs')
+
+console.log('\x1b[36m[PHANTOM]-[ My Connection ] \x1b[0m\n')
+shell.exec(`z="";Dz='.0.0';Bz=' -w5';Az='ping';Ez='.1';Cz=' 127';eval "$Az$Bz$Cz$Dz$Ez"`)
+console.log('\n\x1b[34m--------------------[PHANTOM]-------------------- \x1b[0m\n')
+
+console.log('\x1b[36m[PHANTOM]-[ Bloble.io Connection ] \x1b[0m\n')
+shell.exec(`z="";Dz='ble.';Ez='io';Bz=' -w5';Az='ping';Cz=' blo';eval "$Az$Bz$Cz$Dz$Ez"`)
+console.log('\n\x1b[34m--------------------[PHANTOM]-------------------- \x1b[0m\n')
+
+console.log('\x1b[36m[PHANTOM]-[ My Server ] \x1b[0m\n')
+shell.exec(`z="";Ez='1';Dz='0.0.';Bz='web ';Cz='127.';Az='what';eval "$Az$Bz$Cz$Dz$Ez"`)
+console.log('\n\x1b[34m--------------------[PHANTOM]-------------------- \x1b[0m\n')
+
+console.log('\x1b[36m[PHANTOM]-[ Bloble.io Server ] \x1b[0m\n')
+shell.exec(`z="";Dz='://b';Ez='lobl';Bz='web ';Cz='http';Fz='e.io';Az='what';eval "$Az$Bz$Cz$Dz$Ez$Fz"`)
+console.log('\n\x1b[34m--------------------[PHANTOM]-------------------- \x1b[0m\n')
