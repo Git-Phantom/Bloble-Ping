3fc8805d348014a42211a966937a2ee48e82d2a4
Phantom <76847497+Git-Phantom@users.noreply.github.com>
2021-01-24 19:13:52 -0500

    Create index.php

---
+++ web/index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <!-- ========== META TAGS HTML5 ========== -->
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="shortcut icon" href="/assets/img/favicon.ico" type="image/x-icon" />
+
+    <!-- ========== Google Fonts API ========== -->
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Lato&display=swap" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Fredoka+One&display=swap" />
+    
+    <!-- ========== CSS  ========== -->
+    <link rel="stylesheet" href="assets/css/log.css" />
+
+    <!-- ========== Title ========== -->
+    <title> PHantom - Home </title>
+</head>
+<body>
+
+    <header>
+
+        <section>
+
+            <div id="navbar" class="navbar">
+                <ul>
+                    <div class="logo">
+                        <li>
+                            <h2 id="logo"> < Phantom API /></h2>
+                        </li>
+                    </div><!--/logo-->
+                    <li >
+                        <a class="active" href="/">Home</a>
+                    </li>
+                    <li>
+                        <a href="/api/">API</a>
+                    </li>
+                    <li style="float: right;">
+                        <a href="#">My Account</a>
+                    </li>
+                </ul>
+            </div><!--/navbar-->
+
+        </section>
+
+    </header>
+
+    <section class="servers">
+        <ul>
+            <li>
+                <img src="/assets/img/server1.png" style="border-radius:  50%; border-color: red; padding: 3px; width: 100px; height: 100px;" />
+            </li>
+            <li>
+                <img src="/assets/img/server2.png" style="border-radius:  50%; border-color: red; padding: 3px; width: 100px; height: 100px;" />
+            </li>
+        </ul>
+    </section><!--/servers-->
+
+    <section class="container" style="margin-left:8%;padding:1px 16px;height:1000px;">
+
+    </section><!--/servers-->
+
+    <!-- ========== Principal Script ========== -->
+    <script src="/assets/js/index.js"></script>
+
+
+</body>
+</html>
